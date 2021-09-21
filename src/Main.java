3122d7c90e09d985274b3b6e2be27e6be3a649a5
DR34M-M4K3R <v.michoux@gmail.com>
2021-09-21 20:45:22 +0200

    It finally works :)

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
-*  License, v. 2.0. If a copy of the MPL was not distributed with this
-*  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-*  License holder: DR34M-M4K3R#7751 */
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *  License holder: DR34M-M4K3R#7751 */
 
 
 
 public class Main {
 
-    static int somme;
-    static int nombre;
-    static int nbofpn;
+
     static long startTime;
     static long endTime;
     static String nombrestr= " ";
     static String filename;
 
+    public static boolean isperfect(int number){
+        int somme = 0;
+
+        for (int ii = 1; ii <= number /2; ii++) {
+
+            if (number % ii == 0) {
+                somme=somme+ii;
+
+            }
+        }
+        return (number==somme);
+
+    }
 
     public static void savefile() throws Exception {
         File file = new File(filename);
@@ -34,53 +45,37 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-        ArrayList<Integer> nombres_parfaits = new ArrayList<Integer>();
+        ArrayList<Integer> nombres_parfaits = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
 
 
         System.out.println("Up to what number do you want to compute the perfect numbers?");
-        nbofpn = sc.nextInt();
-        filename="Perfect_number"+nbofpn+".txt";
+        int max = sc.nextInt();
+        filename="Perfect_number"+ max +".txt";
         FileOutputStream fos2 = new FileOutputStream(filename, true);
         fos2.write("e".getBytes());
         fos2.close();
 
 
-        System.out.println("The perfect numbers found will be saved in a text file \nnamed \"Perfect_number"+nbofpn+".txt\" in the same directory as the perfectNumberCalculator.jar file. \nFrom which number to start?");
-        nombre = sc.nextInt();
+        System.out.println("The perfect numbers found will be saved in a text file \nnamed \"Perfect_number"+ max +".txt\" in the same directory as the perfectNumberCalculator.jar file. \nFrom which number to start?");
+        int min = sc.nextInt();
 
 
         long startTime;
         long endTime;
 
         startTime = System.currentTimeMillis();
-        for (int i = nombre; i <= nbofpn; i++){
+        for (int i = min; i <= max; i++){
 
-            for (int ii = 1; ii <= nombre/2; ii++) {
-
-                if (i % ii == 0) {
-                    System.out.println(ii);
-                    somme=somme+ii;
-
-                }
-
-                if (i==somme){
-                    System.out.println(i+", la somme de ses diviseurs fait "+somme);
+                if (isperfect(i)){
                     nombres_parfaits.add(i);
                     nombrestr = String.valueOf(nombres_parfaits);
                     savefile();
                     System.out.println(i);
-                    break;
+                    //break;
 
                 }
-
-
-            }
-            somme = 0;
-
-
-            nombre++;
 
 
         }
@@ -90,7 +85,7 @@ public class Main {
         endTime = System.currentTimeMillis();
 
 
-        System.out.println("Perfect_number"+nbofpn+".txt saved. The perfect numbers up to " + nbofpn + " have been found in "+(endTime - startTime)+" ms.");
+        System.out.println("Perfect_number"+ max +".txt saved. The perfect numbers up to " + max + " have been found in "+(endTime - startTime)+" ms.");
 
 
     }
